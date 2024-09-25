04768a6ff2238642e06d60288eb239f79cec18aa
MaxAFriedrich <58374387+MaxAFriedrich@users.noreply.github.com>
2024-09-25 02:25:13 +0100

    add docker support

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM python:3.11
+
+WORKDIR /app
+
+COPY pyproject.toml poetry.lock ./
+
+RUN pip install poetry && poetry install --no-root
+
+COPY . .
+
+EXPOSE 8080
+
+CMD ["poetry", "run", "python", "server.py"]
--- server.py
+++ server.py
@@ -137,7 +137,7 @@ class RequestHandler(BaseHTTPRequestHandler):
 
 
 def run(server_class=HTTPServer, handler_class=RequestHandler, port=8080):
-    server_address = ('localhost', port)
+    server_address = ('', port)
     httpd = server_class(server_address, handler_class)
     print(f'Starting httpd server on port {port}')
     httpd.serve_forever()
